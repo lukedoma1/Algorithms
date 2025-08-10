a4eaf20e0b79b3058fdce41249114ed54a68b72a
lukedoma1 <lukedoma1@gmail.com>
2025-08-10 12:46:20 -0500

    MergeSort first push

---
+++ Python_Practice/Merge_Sort.py
@@ -0,0 +1,16 @@
+def MergeSort(arr):
+    pass
+
+def PrintArray(array):
+    print()
+    for item in array:
+        print(item + " ")
+    print()
+
+
+array = {1, 9, 10, 8, 5, 8, 3, 5, 4, 2}
+
+
+PrintArray(array)
+MergeSort(array)
+PrintArray(array)
