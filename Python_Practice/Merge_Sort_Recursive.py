ebfd2fd46c9bb974fe9abc678906681f2d9e883b
lukedoma1 <lukedoma1@gmail.com>
2025-08-13 18:56:46 -0500

    finished merge sort recursive

--- Python_Practice/Merge_Sort_Recursive.py
+++ Python_Practice/Merge_Sort_Recursive.py
@@ -1,7 +1,34 @@
 #Sorts in O(nlog(n)) time
 def MergeSort(arr):
-    pass
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2 
 
+    leftHalf = MergeSort(arr[:mid])
+    rightHalf = MergeSort(arr[mid:])
+
+    return Merge(leftHalf, rightHalf)
+
+        
+#MergeSort helper function
+def Merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    
+    #make sure all left over elements are added
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged
         
 
 def PrintArray(array):
@@ -11,9 +38,11 @@ def PrintArray(array):
     print()
 
 
-array = [1, 9, 10, 8, 5, 8, 3, 5, 4, 2]
+array = [1, 9, 10, 8, 5, 8, 23, 20, 
+         19, 15, 14, 19, 16, 15, 14, 
+         2, 14, 2, 142, 14, 5, 4, 2]
 
 
 PrintArray(array)
-MergeSort(array)
+array = MergeSort(array)
 PrintArray(array)
